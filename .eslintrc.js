1a451d07a613264ef15f48c563898eb53c42971a
Mehmet Arslan <mehmet.arslan@outlook.at>
2023-10-30 03:03:57 +0100

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,11 +9,7 @@ module.exports = {
         ecmaVersion: 2017,
     },
     rules: {
-        indent: ['error', 8], 
-        'linebreak-style': ['error', 'unix'],
-        'quotes': ['error', 'single'],
-        'semi': ['error', 'always'],
-        'no-undef': 'off', 
+        'quotes': ['error', 'single'], // Überprüft nur die Regel für einzelne Anführungszeichen
     },
     globals: {
         module: 'readonly',
