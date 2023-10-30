60c4a33d7ab1378c4cffa9e10b0a5ad82a047585
Mehmet Arslan <mehmet.arslan@outlook.at>
2023-10-30 07:46:27 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
-const path = require('path');
-export default {
+import { defineConfig } from 'vite';
+
+export default defineConfig({
   build: {
+    target: 'modules',
+    outDir: 'dist', // Output directory for the build
+    assetsDir: 'assets', // Directory for static assets (e.g., CSS, images)
+    assetsInlineLimit: 4096, // Inline assets smaller than this size (in bytes)
+    minify: true, // Minify the output
+    sourcemap: false, // Generate source maps
     rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-        about: path.resolve(__dirname, 'pages/about.html'),
-        form: path.resolve(__dirname, 'pages/form.html'),
-        favorites: path.resolve(__dirname, 'pages/favorites.html')
+      output: {
+        entryFileNames: 'js/[name]-[hash].js', // Output format for entry files
+        chunkFileNames: 'js/[name]-[hash].js', // Output format for chunk files
+        assetFileNames: 'assets/[name]-[hash][extname]', // Output format for asset files
       },
     },
   },
-};
+});
