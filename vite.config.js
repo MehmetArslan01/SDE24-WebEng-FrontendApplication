c0547daca7205e807ed708c60d7d2a1425f04647
Mehmet Arslan <mehmet.arslan@outlook.at>
2023-10-30 07:27:39 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
+const path = require('path');
 export default {
-    build: {
-      minify: true,
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        about: path.resolve(__dirname, 'pages/about.html'),
+        form: path.resolve(__dirname, 'pages/form.html'),
+        favorites: path.resolve(__dirname, 'pages/favorites.html')
+      },
     },
-    base: "/SDE24-WebEng-FrontendApplication/",
-  };
+  },
+};
