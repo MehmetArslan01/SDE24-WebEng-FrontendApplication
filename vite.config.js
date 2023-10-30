f0105cd1a541c200c535c23a8541d7fe18d1b51b
Mehmet Arslan <mehmet.arslan@outlook.at>
2023-10-30 07:51:25 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/SDE24-WebEng-FrontendApplication/', // Set your deployment base path
+
   build: {
-    target: 'modules',
-    outDir: 'dist', // Output directory for the build
-    assetsDir: 'assets', // Directory for static assets (e.g., CSS, images)
-    assetsInlineLimit: 4096, // Inline assets smaller than this size (in bytes)
-    minify: true, // Minify the output
-    sourcemap: false, // Generate source maps
     rollupOptions: {
-      output: {
-        entryFileNames: 'js/[name]-[hash].js', // Output format for entry files
-        chunkFileNames: 'js/[name]-[hash].js', // Output format for chunk files
-        assetFileNames: 'assets/[name]-[hash][extname]', // Output format for asset files
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'pages/about.html'),
+        form: resolve(__dirname, 'pages/form.html'),
+        favorites: resolve(__dirname, 'pages/favorites.html'),
       },
     },
   },
